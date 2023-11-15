d6a1c1c0ff251befa0e981272e658446e1685f4a
SJD-PC\sjd <admin@hotxf.com>
2023-11-15 20:27:50 +0800

    no message

---
+++ app/home/controller/Index.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\home\controller;
+
+use think\facade\View;
+use think\facade\Db;
+use fast\Page;
+class Index 
+{
+
+    public function index()
+    {
+
+       
+        return '小风博客www.hotxf.com';
+
+    }
+   
+    
+}
